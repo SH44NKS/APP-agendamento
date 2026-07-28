cb117900ecf653e4b486baec5dc55bc99161f038
SH44NKS <alissons.silva25@gmail.com>
2026-07-28 13:33:57 -0300

    adiciona verificação do Google Search Console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,3 @@
 import type {Metadata} from "next";import "./globals.css";
-export const metadata:Metadata={title:"APP agendamento | Foco & Escudo",description:"Sistema interno para controle operacional de instalações, retiradas e manutenções."};
+export const metadata:Metadata={title:"APP agendamento | Foco & Escudo",description:"Sistema interno para controle operacional de instalações, retiradas e manutenções.",verification:{google:"_Z2Ed8XfOtqqPK_iiOz1aXk4Dvr-dH8jOu3Hpuxuz04"}};
 export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="pt-BR"><body>{children}</body></html>}
